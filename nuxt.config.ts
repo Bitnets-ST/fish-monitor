a7314497bff692c580cdbb7d094ce35f67362fed
Nacho <105531940+NachoOFC@users.noreply.github.com>
2025-05-06 15:27:47 -0400

    error tailwindd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/image', '@nuxtjs/tailwindcss']
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss'
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  }
 })
