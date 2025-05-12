7e0ad55c85622daf3dfce42f6d71c22ea9f3b13b
Zosfox <75467523+Zosfox@users.noreply.github.com>
2025-05-12 12:01:14 -0400

    de modifica el nombre dde camaras a lucas
    fsdfsfgqrdrhsdfg

--- pages/cameras.vue
+++ pages/cameras.vue
@@ -3,12 +3,12 @@
     <div class="max-w-7xl mx-auto">
       <div class="flex justify-between items-center mb-6">
         <div>
-          <h1 class="text-2xl font-bold text-gray-800 dark:text-white">Cámaras</h1>
+          <h1 class="text-2xl font-bold text-gray-800 dark:text-white">Cámaras de lucas</h1>
           <p class="text-sm text-gray-600 dark:text-gray-400">Sistema de monitoreo en vivo de estanques por zonas</p>
-        </div>
+        </div>sdf
         <button 
           class="flex items-center text-sm bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition-colors shadow-md ml-auto" 
-          @click="goBack"
+          @click="goBack"ad
         >
           <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clip-rule="evenodd" />
